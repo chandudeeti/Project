1f105a1d29ec6d2313c23b8e7f45731ba91df0cc
Chandu <nanipatel112@gmail.com>
2024-01-04 12:49:36 +0530

    Commit

---
+++ src/main/java/com/infinite/Project/POJO/User.java
@@ -0,0 +1,37 @@
+package com.infinite.Project.POJO;
+
+import lombok.Data;
+import lombok.Generated;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "contactNumber")
+    private String contactNumber;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "status")
+    private String status;
+
+}
